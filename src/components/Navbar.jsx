3f2eef3281efa43afd9b3b3837cb7705b3853b09
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-30 18:19:17 +0000

    Add Navbar component skeleton

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button, Menu, Typography, Avatar} from 'antd'
+
+ const Navbar = ()=>{
+    return (
+        <div>
+            <p>Navigation</p>
+        </div>
+    )
+}
+
+export default Navbar;
