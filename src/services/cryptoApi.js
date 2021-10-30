d5ea7c0bb8704f165ad2705069d25ac861afac35
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-30 23:55:54 +0000

    Add CryptoApi service methods

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,20 @@
-// var axios = require("axios").default;
-//
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/markets',
-//     headers: {
-//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//         'x-rapidapi-key': '479f71b094msh590611df11313ebp1e80bbjsn612b832d928e'
-//     }
-// };
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders  = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '479f71b094msh590611df11313ebp1e80bbjsn612b832d928e'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints:(builder) =>({
+        getCryptos: builder.query({
+            query: (arg) => createRequest('/exchanges')
+        })
+    })
+})
