59a0ea29ec05077a9684ec8f96282d48811ed04e
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-30 22:52:35 +0000

    Add base for redux toolKit services

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+// var axios = require("axios").default;
+//
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/markets',
+//     headers: {
+//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//         'x-rapidapi-key': '479f71b094msh590611df11313ebp1e80bbjsn612b832d928e'
+//     }
+// };
