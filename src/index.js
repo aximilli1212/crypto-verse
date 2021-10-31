69ebc007a9967559881b814442d919eadcb8dc63
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-31 09:58:29 +0000

    Add And Design import and fetch coins

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import {store } from './app/store'
+import store from './app/store'
+import 'antd/dist/antd.css';
 import App from './App';
 
 
